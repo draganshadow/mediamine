af53dfa2044b83d5fd212479a76f49c1ddabb45a
draganshadow <gc@netsyos.com>
2015-01-11 11:38:11 +0100

    Preprare build

--- docker/mediamine/start.sh
+++ docker/mediamine/start.sh
@@ -4,7 +4,7 @@ docker stop mediamine
 docker rm mediamine
 docker run --name mediamine -d -p 80:80 mediamine/mediamine \
     -v /etc/mediamine/config:/app/config \
-    -v /var/opt/mediamine:/app/data \
+    -v /var/opt/mediamine:/app/db \
     -v /var/log/mediamine:/app/log \
     -v /tmp/mediamine/stream:/app/src/web/stream \
     -v /tmp/mediamine/resized:/app/src/web/images/resized \
